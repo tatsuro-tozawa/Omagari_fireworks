a1ffb5454807587b10f0a0dbb174a62b39dc3080
TatsuroTozawa <tgty0414@gmail.com>
2021-03-03 23:38:48 +0900

    Update_20210303

---
+++ scripts/libs/countdown.js
@@ -0,0 +1,29 @@
+function countdown(due) {
+    const now = new Date();
+
+    const rest = due.getTime() - now.getTime();
+    const sec = Math.floor(rest / 1000) % 60;
+    const min = Math.floor(rest / 1000 / 60) % 60;
+    const hours = Math.floor(rest / 1000 / 60 /60) % 24;
+    const days = Math.floor(rest / 1000 / 60 / 60 / 24);
+    const count = [days, hours, min, sec];
+
+    return count;
+}
+
+const goal = new Date(2021, 8, 28);
+
+function recals() {
+    const counter = countdown(goal);
+    document.getElementById('day').textContent = counter[0];
+    document.getElementById('hour').textContent = counter[1];
+    document.getElementById('min').textContent = String(counter[2]).padStart(2, '0');
+    document.getElementById('sec').textContent = String(counter[3]).padStart(2, '0');
+    refresh();
+}
+
+function refresh() {
+    setTimeout(recals, 1000);
+}
+
+recals();
---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hero = new HeroSlider('.swiper-container');
+    hero.start();
+});
+
+new MobileMenu();

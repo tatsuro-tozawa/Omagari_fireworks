4f94d2c69b1a8ead375edf693dcaa43f76b1dcdf
TatsuroTozawa <tgty0414@gmail.com>
2021-03-12 13:20:42 +0900

    Update_20210312

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // ヒーロースライダー
     const hero = new HeroSlider('.swiper-container');
     hero.start();
+
+    // // 文字・画像アニメーション 
+    const cb = function(el, inview) {
+        if(inview) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    }
+    const so = new ScrollObserver('.tween-animate-title', cb);
+
+    const _inviewAnimation = function(el, inview) {
+        if(inview) {
+            el.classList.add('inview');
+        }else {
+            el.classList.remove('inview');
+        }
+    }
+    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
 });
 
 new MobileMenu();
